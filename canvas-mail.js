765dbf8885bef9c3328bb56c273d31f8ad374bbb
Erik Brinkman <ebrink@umich.edu>
2014-07-18 01:16:49 -0700

    Fixed coloration and added temporary basis for underlining

--- canvas-mail.js
+++ canvas-mail.js
@@ -8,6 +8,68 @@ var CANVAS_MAIL = (function( $ ){
 	   domain   : email domain name
 	*/
 
+	function measure_text( text, font ) {
+	    var txt = $("<span></span>");
+	    txt.text(text);
+	    txt.css("font", font);
+	    $(document.documentElement).append(txt);
+	    var height = txt.height();
+	    var width = txt.width();
+	    txt.remove()
+	    return {
+		height: height,
+		width: width
+	    };
+	}
+
+	// TODO Allow manual specification of lineheight / thickness
+	function render_text( text, font, font_size, color, decoration ) {
+	    // Create canvas
+	    var size = measure_text(text, font);
+	    var canvas = $("<canvas></canvas>");
+	    canvas.attr(size);
+	    canvas.css("vertical-align", "text-top");
+
+	    // Draw text on canvas
+	    var ctx = canvas[0].getContext("2d");
+	    ctx.font = font;
+	    ctx.fillStyle = color;
+	    ctx.textAlign = "left";
+	    ctx.textBaseline = "top";
+	    ctx.fillText(text, 0, 0);
+
+	    // Draw decoration
+	    arr = decoration.split(" ");
+	    dec_type = arr.shift();
+	    dec_style = arr.shift(); // Nonstandard, ignored
+	    dec_color = arr.join(" "); // Nonstandard, ignored
+
+	    // Switch on line height
+	    var line_height = 0
+	    switch (dec_type) {
+		case "underline":
+		    line_height = font_size;
+		    break;
+		case "overline":
+		    break;
+		case "line-through":
+		    line_height = font_size / 2;
+		case "none":
+		default:
+		    return canvas;
+	    }
+	    
+	    // Draw actual line
+	    ctx.beginPath();
+	    ctx.strokeStyle = color;
+	    ctx.lineWidth = 1;
+	    ctx.moveTo(0, line_height);
+	    ctx.lineTo(size.width, line_height);
+	    ctx.stroke();
+	    
+	    return canvas;
+	}
+
 	elements.each(function() {
 	    var $this = $(this);
 	    
@@ -16,44 +78,22 @@ var CANVAS_MAIL = (function( $ ){
 	    // possible, but this also clears the link. Potentially add the
 	    // link, get the styles, clear and read
 
-	    // Read attributes of the element to duplicate style
-	    var fontSize = parseInt($this.css("font-size"));
-	    var fontFamily = $this.css("font-family");
-	    var color = $this.css("color");
-	    
-	    var height = parseInt($this.css("height"))
-	    if (height == 0) { // Empty line -> guess
-		height = Math.floor(fontSize * 1.5)
-	    }
-	    // TODO does this work with em?
-	    fontText = fontSize + "px " + fontFamily;
-	    
-	    // Get rendred text width
-	    var testCtx = $("<canvas></canvas>")[0].getContext("2d");
-	    testCtx.font = fontText;
-	    var width = testCtx.measureText(user + "@" + domain).width;
-	    
-	    // Create proper sized canvas
-	    var canvas = $("<canvas></canvas>");
-	    canvas.attr({"width" : width, "height" : height});
-	    canvas.css("vertical-align", "text-top");
-	    
-	    // Draw text on canvas
-	    var ctx = canvas[0].getContext("2d");
-	    ctx.font = fontText;
-	    ctx.fillStyle = color;
-	    ctx.textAlign = "left";
-	    ctx.textBaseline = "top";
-	    ctx.fillText(user + "@" + domain, 0, 0);
-	    
+	    var canvas = render_text(
+		user + "@" + domain,
+		$this.css("font"),
+		parseInt($this.css("font-size")),
+		$this.css("color"),
+		$this.css("text-decoration")
+	    );
+
 	    $this.empty();
-	    
+	    $this.append(canvas);
+
 	    if ($this.prop("tagName") == "A") {
 		$this.attr({"onmouseover" : ("this.href='mailto:' + '" + user +
 					     "' + '@' + '" + domain + "'"),
 	    		    "onmouseout" : "this.href=null"});
 	    }
-	    $this.append(canvas);
 	});
     };
 
